14ea00abb6b49cab5c7dd7fbc2ddd9eba86ff2ea
Roxanna <rxnpjr@gmail.com>
2015-01-21 19:27:45 -0500

    ejercicio 2

---
+++ ejerciciofeo.rb
@@ -0,0 +1,25 @@
+# puts "escribir primer numero"
+# primernumero=gets.chomp.to_i
+# puts "segundo numero"
+# segundonumero=gets.chomp.to_i
+# puts "tercer numero"
+# tercernumero=gets.chomp.to_i
+
+# if primernumero > segundonumero
+# 	puts "el numero mayor es #{primernumero}"
+# elsif 
+
+# puts "ponga tres numeros"
+
+# numeros=[gets.chomp.to_i]
+
+# numeros.sort
+# puts numeros[0]
+puts "ponga tres numeros"
+a = [ gets.chomp.to_i, gets.chomp.to_i, gets.chomp.to_i ]
+
+respuesta=a.sort  
+
+puts respuesta
+
+
